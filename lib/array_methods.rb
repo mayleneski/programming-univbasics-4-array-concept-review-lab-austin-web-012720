8854eb65e1c75f58c3653dfee99e3e2a853efc4a
Maylene Poulsen <mayleneski@hotmail.com>
2019-11-30 18:32:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,13 @@
 def find_element_index(array, value_to_find)
-  counter = 0 
-  while array.include?(value_to_find) do
-    puts counter
-    counter += 1
+  
   end
   
 
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort!
+  puts 
 end
 
 def find_min_value(array)
